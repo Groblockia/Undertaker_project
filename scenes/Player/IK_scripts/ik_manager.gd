72addb9643252913c1b0f584f77c5f83b8cdd005
Groblockia <groblovroum@gmail.com>
2026-01-22 14:42:53 +0100

    need to add step_logic and magnets for pointing leg upwards

--- scenes/Player/IK_scripts/ik_manager.gd
+++ scenes/Player/IK_scripts/ik_manager.gd
@@ -1,3 +1,4 @@
+
 extends Node3D
 
 @onready var parent := $".."
@@ -19,7 +20,7 @@ extends Node3D
 @onready var magnet_Bk_R := $Magnets/BackRightMagnet
 
 # position of ik targets
-@export var width: float  = 1.5 # left/right distance
+@export var width: float  = 2.5 # left/right distance
 @export var length: float = 2.0 # front/back distance
 @export var height: float = 0.5 # up/down distance
 @export var Zoffset: float = 0.0 # offset everything front/back
@@ -27,11 +28,20 @@ extends Node3D
 # misc
 @export var position_speed: float = 5.0
 
+@export var magnet_height: float = 2.5  # How high the "knee" is pulled
+@export var magnet_outward_bias: float = 0.5 # Pushes knees away from the body
+
 func _ready():
-	setup_marker_positions()
+	if Engine.is_editor_hint():
+		setup_marker_positions()
+	else:
+		setup_marker_positions()
 
 func _process(delta: float) -> void:
-	calculate_marker_positions(delta)
+	if Engine.is_editor_hint():
+		calculate_marker_positions(delta)
+	else:
+		calculate_marker_positions(delta)
 
 func setup_marker_positions():
 	ik_targets_container.rotation = head.rotation
@@ -43,7 +53,8 @@ func setup_marker_positions():
 
 func calculate_marker_positions(delta: float):
 	
-	ik_targets_container.rotation = lerp(ik_targets_container.rotation, head.rotation, delta * position_speed)
+	ik_targets_container.rotation.y = lerp_angle(ik_targets_container.rotation.y, head.rotation.y, delta * position_speed)
+	magnets_container.rotation.y 	= lerp_angle(magnets_container.rotation.y, head.rotation.y, delta * position_speed)
 
 	# direction * basis = local to global, direction * basis.inverse() = global to local
 	var local_lean = ik_targets_container.global_transform.basis.inverse() * parent.direction
@@ -51,7 +62,7 @@ func calculate_marker_positions(delta: float):
 	front_left.position = lerp(front_left.position, 
 		( Vector3(-width, height, -length) + Vector3(0,0,Zoffset) ) + (local_lean), 
 		delta * position_speed)
-	
+
 	front_right.position = lerp(front_right.position, 
 		( Vector3(width, height, -length) + Vector3(0,0,Zoffset) ) + (local_lean), 
 		delta * position_speed)
@@ -63,3 +74,4 @@ func calculate_marker_positions(delta: float):
 	back_right.position = lerp(back_right.position, 
 		( Vector3(width, height, length) + Vector3(0,0,Zoffset) ) + (local_lean), 
 		delta * position_speed)
+
