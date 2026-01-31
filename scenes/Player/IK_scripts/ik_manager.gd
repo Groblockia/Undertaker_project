ca26ffb05ef9cd09b121093d3b63dd744b80d384
Groblockia <groblovroum@gmail.com>
2026-01-31 21:42:24 +0100

    update to 4.6 stable

--- scenes/Player/IK_scripts/ik_manager.gd
+++ scenes/Player/IK_scripts/ik_manager.gd
@@ -1,4 +1,3 @@
-
 extends Node3D
 
 @onready var parent := $".."
@@ -11,6 +10,13 @@ extends Node3D
 @onready var front_right := $IkTargets/FrontRightMarker
 @onready var back_left   := $IkTargets/BackLeftMarker
 @onready var back_right  := $IkTargets/BackRightMarker
+
+# Ik homes
+@onready var ik_current_container := $IkCurrentTargets
+@onready var front_left_current  := $IkCurrentTargets/FrontLeftCurrent
+@onready var front_right_current := $IkCurrentTargets/FrontRightCurrent
+@onready var back_left_current   := $IkCurrentTargets/BackLeftCurrent
+@onready var back_right_current  := $IkCurrentTargets/BackRightCurrent
 
 # magnets to keep middle joints upwards
 @onready var magnets_container := $Magnets
@@ -28,36 +34,27 @@ extends Node3D
 # misc
 @export var position_speed: float = 5.0
 
-@export var magnet_height: float = 2.5  # How high the "knee" is pulled
-@export var magnet_outward_bias: float = 0.5 # Pushes knees away from the body
-
-func _ready():
-	if Engine.is_editor_hint():
-		setup_marker_positions()
-	else:
-		setup_marker_positions()
+func _ready() -> void:
+	setup_marker_positions()
 
 func _process(delta: float) -> void:
-	if Engine.is_editor_hint():
-		calculate_marker_positions(delta)
-	else:
-		calculate_marker_positions(delta)
+	calculate_marker_positions(delta)
+	step()
 
-func setup_marker_positions():
-	ik_targets_container.rotation = head.rotation
+func setup_marker_positions() -> void:
+	#ik_targets_container.rotation = head.rotation
 	# Position = Vector3(left/right, height, front/back)
 	front_left.position  = ( Vector3(-width, height, -length) + Vector3(0,0,Zoffset) )
 	front_right.position = ( Vector3(width, height, -length) + Vector3(0,0,Zoffset) )
 	back_left.position   = ( Vector3(-width, height, length) + Vector3(0,0,Zoffset) )
 	back_right.position  = ( Vector3(width, height, length) + Vector3(0,0,Zoffset) )
 
-func calculate_marker_positions(delta: float):
-	
+func calculate_marker_positions(delta: float) -> void:
 	ik_targets_container.rotation.y = lerp_angle(ik_targets_container.rotation.y, head.rotation.y, delta * position_speed)
 	magnets_container.rotation.y 	= lerp_angle(magnets_container.rotation.y, head.rotation.y, delta * position_speed)
 
 	# direction * basis = local to global, direction * basis.inverse() = global to local
-	var local_lean = ik_targets_container.global_transform.basis.inverse() * parent.direction
+	var local_lean = ik_targets_container.global_transform.basis.inverse() * ((parent.velocity * 0.5).clamp(Vector3(-1.5, -1.5, -1.5), Vector3(1.5, 1.5, 1.5)))
 
 	front_left.position = lerp(front_left.position, 
 		( Vector3(-width, height, -length) + Vector3(0,0,Zoffset) ) + (local_lean), 
@@ -66,12 +63,17 @@ func calculate_marker_positions(delta: float):
 	front_right.position = lerp(front_right.position, 
 		( Vector3(width, height, -length) + Vector3(0,0,Zoffset) ) + (local_lean), 
 		delta * position_speed)
-	
+
 	back_left.position = lerp(back_left.position, 
 		( Vector3(-width, height, length) + Vector3(0,0,Zoffset) ) + (local_lean), 
 		delta * position_speed)
-	
+
 	back_right.position = lerp(back_right.position, 
 		( Vector3(width, height, length) + Vector3(0,0,Zoffset) ) + (local_lean), 
 		delta * position_speed)
 
+func step():
+	front_left_current.position  = front_left.global_position
+	front_right_current.position = front_right.global_position
+	back_left_current.position   = back_left.global_position
+	back_right_current.position  = back_right.global_position
